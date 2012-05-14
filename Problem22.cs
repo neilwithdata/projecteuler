069a240603385437f4a94c2e87f066d1b76a512e
Neil Sainsbury <neil.sainsbury@gmail.com>
2012-05-14 13:57:07 -0400

    Added solution for problem 22

---
+++ Problem22.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Problem22
+{
+    static void Main(string[] args)
+    {
+        string fileText = System.IO.File.ReadAllText("problem22_data.txt");
+        string[] names = fileText.Split(new char[] { '"', ',' },
+            StringSplitOptions.RemoveEmptyEntries);
+
+        Array.Sort(names);
+
+        int totalScore = 0;
+        for (int i = 0; i < names.Length; i++)
+        {
+            int nameValue = 0;
+            char[] nameArray = names[i].ToCharArray();
+            
+            for (int j = 0; j < nameArray.Length; j++)
+                nameValue += (int)nameArray[j] - 64;
+
+            nameValue *= (i + 1);
+            totalScore += nameValue;
+        }
+
+        Console.WriteLine(totalScore);
+    }
+}
