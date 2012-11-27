742bf919282a1b31f04d94eb10bdb9d3e08ba4ef
Neil Sainsbury <neil.sainsbury@gmail.com>
2012-11-27 12:22:01 +1100

    added solutions to problem 25, 29

---
+++ Problem25.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Numerics;
+
+class Problem25
+{
+    static void Main(string[] args)
+    {
+        BigInteger[] previous = new BigInteger[] { 1, 1 };
+        int termNo = 2;
+        BigInteger next;
+
+        while (true)
+        {
+            next = previous[0] + previous[1];
+            termNo++;
+
+            if (next.ToString().Length >= 1000)
+                break;
+            else
+            {
+                previous[0] = previous[1];
+                previous[1] = next;
+            }
+        }
+
+        Console.WriteLine(termNo);
+    }
+}
---
+++ Problem29.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+class Problem29
+{
+    static void Main(string[] args)
+    {
+        // Maximum sequence length
+        int sequenceLength = 99 * 99;
+
+        // for each a^b, look for higher base representations
+        // We can dramatically simply this search knowing that if there are other y^z representations of a^b,
+        // they must satisfy
+        //      b = log_a(y^z)
+        //      b = z.log_a(y), where y and z are integers
+
+        for (int a = 2; a <= 10; a++)
+        {
+            for (int b = 2; b <= 100; b++)
+            {
+                // Begin the search for any higher base representations of a^b
+                // for each power of the base <= 100
+                int pow = 2;
+                int y = (int) Math.Pow(a, pow);
+                while (y <= 100)
+                {
+                    if (b % pow == 0)
+                    {
+                        int z = b / pow;
+                        if (z >= 2 && z <= 100)
+                        {
+                            Console.WriteLine("{0}^{1} == {2}^{3}", a, b, y, z);
+                            // evenly divides...we have our y and z
+                            sequenceLength--;
+                        }
+                    }
+
+                    y = (int) Math.Pow(a, ++pow);
+                }
+
+            }
+        }
+
+        Console.WriteLine("Sequence Length: {0}", sequenceLength);
+        Console.ReadLine();
+    }
+}
